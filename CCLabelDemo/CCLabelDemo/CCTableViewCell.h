40c71f4287111b6a9bb5745d2e8ad4bb4dbb22e7
CC <ccworld1000@gmail.com>
2019-07-02 18:05:53 +0800

    add spec

---
+++ CCLabelDemo/CCLabelDemo/CCTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  CCTableViewCell.h
+//  CCLabelDemo
+//
+//  Created by dengyouhua on 2019/7/2.
+//  Copyright © 2019 cc | ccworld1000@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CCTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CCLabelDemo/CCLabelDemo/CCTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  CCTableViewCell.m
+//  CCLabelDemo
+//
+//  Created by dengyouhua on 2019/7/2.
+//  Copyright © 2019 cc | ccworld1000@gmail.com. All rights reserved.
+//
+
+#import "CCTableViewCell.h"
+
+@implementation CCTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
